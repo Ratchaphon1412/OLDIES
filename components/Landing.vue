30bb58018620976cbc8ff68c5bc2e0ca82dbfddb
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-04-10 15:22:01 +0700

    add id home for navbar scroll to

--- components/Landing.vue
+++ components/Landing.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="overflow-hidden">
+  <section id="home" class="overflow-hidden">
     <div class="relative overflow-hidden background-city h-screen">
       <div
         class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
