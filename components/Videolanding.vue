d3101d693cc7d1b9231903cdfb41859da255bb0a
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-04-10 18:22:55 +0700

    up speed video background

--- components/Videolanding.vue
+++ components/Videolanding.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
-    <video autoplay loop muted playsinline>
+    <video ref="bangkok" autoplay loop muted playsinline>
       <source src="video/Bangkok.mp4" type="video/mp4" />
     </video>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'VideoBangkok',
+  mounted() {
+    const video = this.$refs.bangkok
+    video.playbackRate = 2
+  },
+}
+</script>
 
 <style scoped>
 video {
