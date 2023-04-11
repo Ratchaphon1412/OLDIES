1844691a572268445ef73fc92ae49b286c04e5cf
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-04-11 15:08:23 +0700

    add animation config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,18 @@ module.exports = {
       serif: ['Merriweather', 'serif'],
     },
     extend: {
+      animation: {
+        fadeIn: "fadeIn 2s ease-in forwards"
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        }
+      },
+      variants: {
+        animation: ["motion-safe"]
+    },
       spacing: {
         '128': '32rem',
         '144': '36rem',
