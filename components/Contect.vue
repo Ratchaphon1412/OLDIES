2b2b2c5453e03b5a81a4d4e04d8d16754b3e9e7a
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-04-11 17:30:21 +0700

    change color

--- components/Contect.vue
+++ components/Contect.vue
@@ -48,7 +48,7 @@
           <textarea
             id="message"
             rows="6"
-            class="block p-2.5 w-full text-sm rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            class="block p-2.5 w-full text-sm rounded-lg shadow-sm border focus:ring-primary-500 focus:border-primary-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500"
             placeholder="Leave a comment..."
           ></textarea>
         </div>
